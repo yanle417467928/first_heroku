892ce4558349dcf704920b3779ebc42ed0ca675e
leyan <417467928@qq.com>
2017-08-28 14:42:41 +0800

    初始提交

---
+++ src/main/java/com/yanle/first_heroku/FirstHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.yanle.first_heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstHerokuApplication.class, args);
+	}
+}
